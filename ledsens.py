967dd0ebf3895ff26e86e279de1863e6a1063584
roadyweb <nomail@nomail.no>
2018-09-04 17:47:04 +0200

    calc average values to check measurement variation

--- ledsens.py
+++ ledsens.py
@@ -4,6 +4,7 @@
 from __future__ import print_function
 
 import RPi.GPIO as GPIO
+import numpy
 import time
 from string import upper
 
@@ -12,8 +13,8 @@ from string import upper
 s2 = 23
 s3 = 24
 signal = 25
-NUM_CYCLES = 100
-WAIT_AFT_SWITCH = 0.3
+NUM_CYCLES = 20
+WAIT_AFT_SWITCH = 0.0
 
 
 def set(color):
@@ -41,7 +42,7 @@ def measure():
         GPIO.wait_for_edge(signal, GPIO.FALLING)
     duration = time.time() - start      # seconds to run for loop
     color = NUM_CYCLES / duration         # in Hz
-    return color
+    return color / 50
 
 
 def setup():
@@ -49,25 +50,37 @@ def setup():
     GPIO.setup(signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(s2, GPIO.OUT)
     GPIO.setup(s3, GPIO.OUT)
+    time.sleep(0.3)
     print("\n")
 
 
 def loop():
     while 1:
-        set('RED')
-        red = measure()
+        arr_red = []
+        arr_green = []
+        arr_blue = []
+    
+        for i in range(10):
+            set('RED')
+            red = measure()
+            arr_red.append(red)
+    
+            set('BLUE')
+            blue = measure()
+            arr_blue.append(blue)
+    
+            set('GREEN')
+            green = measure()
+            arr_green.append(green)
 
-        set('BLUE')
-        blue = measure()
-
-        set('GREEN')
-        green = measure()
-
-        set('CLEAR')
-        clear = measure()
-
-        print('R: %5d, G: %5d, B: %5d C: %5d' %
-              (red, green, blue, clear))
+            # print('R: %5d, G: %5d, B: %5d' %
+            #       (red, green, blue))
+    
+        print('R: %5d (+/-%5d), G: %5d  (+/-%5d), B: %5d  (+/-%5d)' %
+              (numpy.mean(arr_red), numpy.std(arr_red),
+               numpy.mean(arr_green), numpy.std(arr_green),
+               numpy.mean(arr_blue), numpy.std(arr_blue))
+        )
 
 
 def endprogram():
