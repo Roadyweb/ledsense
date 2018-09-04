44a5b3077606aea109f5c14973621e42c381e5a1
roadyweb <nomail@nomail.no>
2018-09-04 16:48:27 +0200

    some code cleanup and refactoring

--- ledsens.py
+++ ledsens.py
@@ -9,58 +9,62 @@ import time
 s2 = 23
 s3 = 24
 signal = 25
-NUM_CYCLES = 10
-
+NUM_CYCLES = 100
+WAIT_AFT_SWITCH = 0.1
 
 def setup():
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(signal,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    GPIO.setup(s2,GPIO.OUT)
-    GPIO.setup(s3,GPIO.OUT)
+    GPIO.setup(signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(s2, GPIO.OUT)
+    GPIO.setup(s3, GPIO.OUT)
     print("\n")
 
 
 def loop():
-    temp = 1
-    while(1):  
+    while(1):
 
-        GPIO.output(s2,GPIO.LOW)
-        GPIO.output(s3,GPIO.LOW)
-        time.sleep(0.3)
+        GPIO.output(s2, GPIO.LOW)
+        GPIO.output(s3, GPIO.LOW)
+        time.sleep(WAIT_AFT_SWITCH)
         start = time.time()
-        for impulse_count in range(NUM_CYCLES):
+        for _ in range(NUM_CYCLES):
             GPIO.wait_for_edge(signal, GPIO.FALLING)
-        duration = time.time() - start      #seconds to run for loop
-        red  = NUM_CYCLES / duration   #in Hz
-        print("red value - ",red)
+        duration_red = time.time() - start      # seconds to run for loop
+        red = NUM_CYCLES / duration_red         # in Hz
+        # print("red value - ",red)
 
-        GPIO.output(s2,GPIO.LOW)
-        GPIO.output(s3,GPIO.HIGH)
-        time.sleep(0.3)
+        GPIO.output(s2, GPIO.LOW)
+        GPIO.output(s3, GPIO.HIGH)
+        time.sleep(WAIT_AFT_SWITCH)
         start = time.time()
-        for impulse_count in range(NUM_CYCLES):
+        for _ in range(NUM_CYCLES):
             GPIO.wait_for_edge(signal, GPIO.FALLING)
-        duration = time.time() - start
-        blue = NUM_CYCLES / duration
-        print("blue value - ",blue)
+        duration_blue = time.time() - start
+        blue = NUM_CYCLES / duration_blue
+        # print("blue value - ",blue)
 
-        GPIO.output(s2,GPIO.HIGH)
-        GPIO.output(s3,GPIO.HIGH)
-        time.sleep(0.3)
+        GPIO.output(s2, GPIO.HIGH)
+        GPIO.output(s3, GPIO.HIGH)
+        time.sleep(WAIT_AFT_SWITCH)
         start = time.time()
-        for impulse_count in range(NUM_CYCLES):
+        for _ in range(NUM_CYCLES):
             GPIO.wait_for_edge(signal, GPIO.FALLING)
-        duration = time.time() - start
-        green = NUM_CYCLES / duration
-        print("green value - ",green)
-        time.sleep(2)  
+        duration_green = time.time() - start
+        green = NUM_CYCLES / duration_green
+        # print("green value - ",green)
+        # time.sleep(2)
+
+        print('R: %5d, G: %5d, B: %5d RD: %5f GD: %5f BD: %5f' %
+              (red, green, blue,
+               duration_red, duration_green, duration_blue))
 
 
 def endprogram():
     GPIO.cleanup()
 
-if __name__=='__main__':
-    
+
+if __name__ == '__main__':
+
     setup()
 
     try:
