ca3b9e58c938c07def56b47d13250c38ae21afeb
roadyweb <nomail@nomail.no>
2018-09-22 16:53:51 +0200

    some code cleanup and refactoring

--- ledsens_tcs34725.py
+++ ledsens_tcs34725.py
@@ -41,53 +41,53 @@ import yaml
 from docopt import docopt
 
 # Uncomment to remote debug
-#import pydevd; pydevd.settrace('192.168.178.80')
+# import pydevd; pydevd.settrace('192.168.178.80')
 
 
-GPIO_LED=4
+GPIO_LED = 4
 
-DEF_CONFIG_FN='config_default.yaml'
+DEF_CONFIG_FN = 'config_default.yaml'
 
-DEF_DESCRIPTION='DEFAULT DESCRIPTION'
+DEF_DESCRIPTION = 'DEFAULT DESCRIPTION'
 
-DEF_DET_THRESHOLD=2
+DEF_DET_THRESHOLD = 2
 
-DEF_RGB_STABLE_CNT=5
-DEF_RGB_STABLE_DIST=10
+DEF_RGB_STABLE_CNT = 5
+DEF_RGB_STABLE_DIST = 10
 
 DEF_COLORS = [
     # Gelbtöne
-    ['RAL 1000 - Grünbeige'    , [4080.0, 3300.0, 2328.0]],
-    ['RAL 1001 - Beige'        , [4188.0, 3079.0, 2263.0]],
-    ['RAL 1002 - Sandgelb'     , [3888.0, 2799.0, 1889.0]],
-    ['RAL 1003 - Signalgelb'   , [4812.0, 2828.0, 1466.0]],
-    ['RAL 1004 - Goldgelb'     , [4172.0, 2535.0, 1356.0]],
-    ['RAL 1005 - Honiggelb'    , [3232.0, 2102.0, 1196.0]],
-    ['RAL 1006 - Maisgelb'     , [4016.0, 2272.0, 1310.0]],
+    ['RAL 1000 - Grünbeige', [4080.0, 3300.0, 2328.0]],
+    ['RAL 1001 - Beige', [4188.0, 3079.0, 2263.0]],
+    ['RAL 1002 - Sandgelb', [3888.0, 2799.0, 1889.0]],
+    ['RAL 1003 - Signalgelb', [4812.0, 2828.0, 1466.0]],
+    ['RAL 1004 - Goldgelb', [4172.0, 2535.0, 1356.0]],
+    ['RAL 1005 - Honiggelb', [3232.0, 2102.0, 1196.0]],
+    ['RAL 1006 - Maisgelb', [4016.0, 2272.0, 1310.0]],
     ['RAL 1007 - Narzissengelb', [4048.0, 2140.0, 1228.0]],
-    ['RAL 1011 - Braunbeige'   , [2764.0, 1849.0, 1347.0]],
-    ['RAL 1012 - Zitronengelb' , [3851.0, 2708.0, 1530.0]],
-    ['RAL 1013 - Perlweiß'     , [5165.0, 4411.0, 3649.0]],
-    ['RAL 1014 - Elfenbein'    , [4541.0, 3593.0, 2631.0]],
+    ['RAL 1011 - Braunbeige', [2764.0, 1849.0, 1347.0]],
+    ['RAL 1012 - Zitronengelb', [3851.0, 2708.0, 1530.0]],
+    ['RAL 1013 - Perlweiß', [5165.0, 4411.0, 3649.0]],
+    ['RAL 1014 - Elfenbein', [4541.0, 3593.0, 2631.0]],
     ['RAL 1015 - Hellelfenbein', [5117.0, 4189.0, 3297.0]],
-    ['RAL 1016 - Schwefelgelb' , [5651.0, 4455.0, 2353.0]],
-    ['RAL 1017 - Safrangelb'   , [4890.0, 2856.0, 1728.0]],
+    ['RAL 1016 - Schwefelgelb', [5651.0, 4455.0, 2353.0]],
+    ['RAL 1017 - Safrangelb', [4890.0, 2856.0, 1728.0]],
     # Blautöne
-    ['RAL 5008 - Graublau'     , [1026.0,  914.0,  842.0]],
-    ['RAL 5009 - Azurblau'     , [1116.0, 1216.0, 1280.0]],
-    ['RAL 5010 - Enzianblau'   , [ 973.0, 1073.0, 1316.0]],
-    ['RAL 5011 - Stahlblau'    , [ 887.0,  783.0,  752.0]],
-    ['RAL 5012 - Lichtblau'    , [1476.0, 1990.0, 2394.0]],
-    ['RAL 5013 - Kobaltblau'   , [ 926.0,  839.0,  878.0]],
-    ['RAL 5014 - Taubenblau'   , [1761.0, 1764.0, 1822.0]],
-    ['RAL 5015 - Himmelblau'   , [1249.0, 1745.0, 2243.0]],
-    ['RAL 5017 - Verkehrsblau' , [ 984.0, 1202.0, 1554.0]],
-    ['RAL 5018 - Türkisblau'   , [1451.0, 1959.0, 1856.0]],
-    ['RAL 5019 - Capriblau'    , [1022.0, 1207.0, 1422.0]],
-    ['RAL 5020 - Ozeanblau'    , [ 894.0,  888.0,  873.0]],
-    ['RAL 5021 - Wasserblau'   , [1114.0, 1494.0, 1449.0]],
-    ['RAL 5022 - Nachtblau'    , [ 952.0,  851.0,  912.0]],
-    ['RAL 5023 - Fernblau'     , [1351.0, 1440.0, 1592.0]],
+    ['RAL 5008 - Graublau', [1026.0, 914.0, 842.0]],
+    ['RAL 5009 - Azurblau', [1116.0, 1216.0, 1280.0]],
+    ['RAL 5010 - Enzianblau', [973.0, 1073.0, 1316.0]],
+    ['RAL 5011 - Stahlblau', [887.0, 783.0, 752.0]],
+    ['RAL 5012 - Lichtblau', [1476.0, 1990.0, 2394.0]],
+    ['RAL 5013 - Kobaltblau', [926.0, 839.0, 878.0]],
+    ['RAL 5014 - Taubenblau', [1761.0, 1764.0, 1822.0]],
+    ['RAL 5015 - Himmelblau', [1249.0, 1745.0, 2243.0]],
+    ['RAL 5017 - Verkehrsblau', [984.0, 1202.0, 1554.0]],
+    ['RAL 5018 - Türkisblau', [1451.0, 1959.0, 1856.0]],
+    ['RAL 5019 - Capriblau', [1022.0, 1207.0, 1422.0]],
+    ['RAL 5020 - Ozeanblau', [894.0, 888.0, 873.0]],
+    ['RAL 5021 - Wasserblau', [1114.0, 1494.0, 1449.0]],
+    ['RAL 5022 - Nachtblau', [952.0, 851.0, 912.0]],
+    ['RAL 5023 - Fernblau', [1351.0, 1440.0, 1592.0]],
 ]
 
 DEF_SENSOR_INTEGRATIONTIME = TCS34725.TCS34725_INTEGRATIONTIME_50MS,
@@ -97,16 +97,15 @@ DEF_CONFIG = {
     'desc': DEF_DESCRIPTION,
     'det': {'threshold': DEF_DET_THRESHOLD},
     'rgb': {
-        'stable_cnt' : DEF_RGB_STABLE_CNT,
+        'stable_cnt': DEF_RGB_STABLE_CNT,
         'stable_dist': DEF_RGB_STABLE_DIST
-     },
+    },
     'color': DEF_COLORS,
     'sensor': {
         'integration_time': DEF_SENSOR_INTEGRATIONTIME,
-        'gain':             DEF_SENSOR_GAIN
+        'gain': DEF_SENSOR_GAIN
     }
 }
-
 
 tcs = None
 
@@ -127,7 +126,7 @@ def measure_rgb(debug=False):
 
 
 def led(on_off):
-    if on_off == True:
+    if on_off:
         led_on()
     else:
         led_off()
@@ -142,10 +141,10 @@ def led_off():
 
 
 def detect_cube(thres=DEF_DET_THRESHOLD):
-    ''' Cube is detected by measuring the clear brightness. If the brightness
+    """ Cube is detected by measuring the clear brightness. If the brightness
         falls below the threshold it is assumed the the cube shields all
         surrounding light, returns the measured clear reading.
-    '''
+    """
     led_off()
     time.sleep(0.1)
     while 42:
@@ -155,10 +154,10 @@ def detect_cube(thres=DEF_DET_THRESHOLD):
 
 
 def detect_cube_removal(thres=DEF_DET_THRESHOLD):
-    ''' Cube is detected by measuring the clear brightness. If the brightness
+    """ Cube is detected by measuring the clear brightness. If the brightness
         falls below the threshold it is assumed the the cube shields all
         surrounding light, returns the measured clear reading.
-    '''
+    """
     led_off()
     time.sleep(0.1)
     while 42:
@@ -168,31 +167,31 @@ def detect_cube_removal(thres=DEF_DET_THRESHOLD):
 
 
 def get_rgb_distance(rgb1, rgb2):
-    ''' This function expects two tuples with RGB values and calculates the distance between both
+    """ This function expects two tuples with RGB values and calculates the distance between both
         vectors.
-    '''
+    """
     rgb1 = numpy.array(rgb1)
     rgb2 = numpy.array(rgb2)
     return numpy.linalg.norm(rgb1 - rgb2)
 
 
 def get_rgb_length(rgb):
-    ''' This function expects one tuples with RGB values and calculates the length of the vector.
-    '''
+    """ This function expects one tuples with RGB values and calculates the length of the vector.
+    """
     return numpy.linalg.norm(rgb)
 
 
 def get_stable_rgb(count=DEF_RGB_STABLE_CNT, dist_limit=DEF_RGB_STABLE_DIST):
-    ''' If a number of consecutive (count) RGB measurements is within a maximum
+    """ If a number of consecutive (count) RGB measurements is within a maximum
         distance (dist_limit) the average of all measurements is calculated and returned.
-    '''
+    """
     while 42:
         res = []
         max_dist = 0
         for i in range(count):
             res.append(measure_rgb(False))
-        for i in range(count-1):
-            act_dist = get_rgb_distance(res[i], res[i+1])
+        for i in range(count - 1):
+            act_dist = get_rgb_distance(res[i], res[i + 1])
             if act_dist > max_dist:
                 max_dist = act_dist
         if max_dist > dist_limit:
@@ -204,9 +203,9 @@ def get_stable_rgb(count=DEF_RGB_STABLE_CNT, dist_limit=DEF_RGB_STABLE_DIST):
 
 
 def get_color(rgb, colors):
-    ''' Takes an RGB list as argument and matches against DEF_COLORS the closest
+    """ Takes an RGB list as argument and matches against DEF_COLORS the closest
         will be uses as match
-    '''
+    """
     min_dist = 999999999
     match = 0
     for color in colors:
@@ -228,7 +227,7 @@ def setup(config):
     gain = config['gain'][0]
     print('Setting TCS config: Integration time: %5d Gain: %5d' % (integration_time, gain))
     tcs = TCS34725.TCS34725(integration_time=integration_time,
-                            gain            =gain,
+                            gain=gain,
                             i2c=1)
 
 
@@ -244,7 +243,7 @@ def app(config_det, config_rgb, config_color):
         led_on()
         time.sleep(0.1)
         res = get_stable_rgb(rgb_stable_cnt, rgb_stable_dist)
-        #print(res)
+        # print(res)
         color = get_color(res, config_color)
         print('%-30s - Distance: %5d - Cur. RGB: %-25s RGB %-20s' %
               (color[0], color[2], str(res), str(color[1])))
@@ -301,7 +300,7 @@ def config_save_default():
 
 
 def config_load(fname):
-    if fname == None:
+    if fname is None:
         print('No config file given. Using default')
         return DEF_CONFIG
 
@@ -323,7 +322,7 @@ def rgb_stable(config_rgb):
 
     while 42:
         res = get_stable_rgb(rgb_stable_cnt, rgb_stable_dist)
-        #print(res)
+        # print(res)
         print('RGB: %25s' % str(res))
 
 
@@ -339,17 +338,17 @@ def main():
     setup(config['sensor'])
 
     try:
-        if args['app'] == True:
+        if args['app']:
             app(config['det'], config['rgb'], config['color'])
-        elif args['detect'] == True:
+        elif args['detect']:
             detect(config['det'])
-        elif args['diff'] == True:
+        elif args['diff']:
             diff()
-        elif args['meas'] == True:
+        elif args['meas']:
             meas(args['on'])
-        elif args['play'] == True:
+        elif args['play']:
             play()
-        elif args['save_default'] == True:
+        elif args['save_default']:
             config_save_default()
         elif args['rgb'] == True and args['stable'] == True:
             rgb_stable(config['rgb'])
