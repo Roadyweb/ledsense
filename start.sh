5a193dff0b42b248decdb8c0a901260e4212a68b
Your Name <you@example.com>
2018-10-05 17:06:10 +0200

    correct path in start.sh, rename ledsense.py

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /home/pi/raspi_ledsens
-./ledsens.py app2 config_cubeholder_black_med_matched.yaml
+cd /home/pi/raspi_ledsense
+./ledsense.py app2 config_cubeholder_black_med_matched.yaml
